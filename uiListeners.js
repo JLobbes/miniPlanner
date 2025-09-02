cd973b22ae10ac8c3817bdede42309ddab8dd107
Jared Lobbestael <jlobbes2@emich.edu>
2025-09-02 23:32:32 +0800

    ADD/STYLES: initial style of project view: title, progress and task lists.

--- uiListeners.js
+++ uiListeners.js
@@ -4,18 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const homeView = document.getElementById('homeView');
-const projectView = document.getElementById('projectView');
+const projectView = document.getElementsByClassName('projectView')[0];
 const newProjBtn = document.querySelectorAll('.newProjectButton')[0];
 
 homeView.addEventListener('click', () => {
-  console.log('home view has been clicked')
-  if (projectView.classList.contains('active')) {
-    projectView.classList.remove('active');
-  }
+  openProjectView();
 });
 
 newProjBtn.addEventListener('click', (e) => {
   e.stopPropagation(); // Prevents the event from bubbling up to homeView
-  console.log('Create a new project!');
-  projectView.classList.add('active');
+  closeProjectView();
 });
+
+function openProjectView() {
+  if (projectView.classList.contains('active')) {
+    projectView.classList.remove('active');
+  }
+}
+
+function closeProjectView() {
+  projectView.classList.add('active');
+}
