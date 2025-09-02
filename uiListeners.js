b13b56a473113fbdc5f12e3eb0679f6b40484575
Jared Lobbestael <jlobbes2@emich.edu>
2025-09-02 13:30:53 +0800

    REFACTOR/ADD: project view drops down after clicking on #newProjectBtn

--- uiListeners.js
+++ uiListeners.js
@@ -1,5 +1,21 @@
 // Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Page loaded and ready!");
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Page loaded and ready!');
 });
 
+const homeView = document.getElementById('homeView');
+const projectView = document.getElementById('projectView');
+const newProjBtn = document.querySelectorAll('.newProjectButton')[0];
+
+homeView.addEventListener('click', () => {
+  console.log('home view has been clicked')
+  if (projectView.classList.contains('active')) {
+    projectView.classList.remove('active');
+  }
+});
+
+newProjBtn.addEventListener('click', (e) => {
+  e.stopPropagation(); // Prevents the event from bubbling up to homeView
+  console.log('Create a new project!');
+  projectView.classList.add('active');
+});
